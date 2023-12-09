b09b0c131baa52c9bef5092cb41785558bae50ee
MendezCarlos-FS <freakinboyo@student.fullsail.edu>
2023-12-08 20:53:12 -0400

    MongoDB setup

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const authorRoutes = require("./authorRoute");
 
 router.get("/", (req, res) => {
     res.status(200).json({ success: true, message: `${req.method} - Request made`});
 });
+
+router.use('/authors', authorRoutes);
 
 module.exports = router;
